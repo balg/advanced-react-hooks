b56a1cfd622d8dc6de57fcb109f96253b097757b
balg <2700278+balg@users.noreply.github.com>
2022-02-02 12:41:29 +0100

    Exercise 1 finished - extra 4

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,21 +4,25 @@
 import * as React from 'react'
 
 const countReducer = (state, action) => {
-  const newState = typeof action === 'function' ? action(state) : action
+  const {type, step} = action
 
-  return {
-    ...state,
-    ...newState,
+  switch (type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + step,
+      }
+    default:
+      throw new Error('Unhandled action type')
   }
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
 
   return <button onClick={increment}>{count}</button>
 }
